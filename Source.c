cdfd0378eb1d8a6afb883809e202bcae8e6b0c6d
liamholland <114741709+liamholland@users.noreply.github.com>
2023-01-31 17:39:58 +0000

    Finding a ship

--- Source.c
+++ Source.c
@@ -1,223 +1,80 @@
 /*
 Name: Liam Holland
 ID: 21386331
-Date: 29-03-22
+Date: 18-01-22
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include <time.h>
+#include <conio.h>
 
-//location struct
-typedef struct {
-	int id, n, s, e, w, in, out;
-	char description[101];
-} location;
+int shipX, shipY;
+int gridSize = 9;	//grid size set to 9 as the last digit of my ID is 1
 
-typedef struct {
-	char name[15];
-	int loc;
-	char des[50];
-} item;
-
-typedef enum {
-	N, S, E, W, IN, OUT,
-	LOOK, QUIT, HELP,
-	INVENTORY, TAKE, DROP, EXAMINE,
-	ERROR
-} command;
-
-location locations[10];	//array for locations
-item items[50];		//array for items
-int numItems;		//number of items scanned in
-char itemName[15];	//variable for the item entered by the user
-FILE* f;
-char commands[][10] = { "n", "s", "e", "w", "in", "out", "look", "quit", "help", "inventory", "take", "drop", "examine" };
-
-void readData();
-void input(location* currL, location* oldL);
-void lower(char arr[]);
-void inv(location* l, int mode, char name[]);
-int parseCom(char input[]);
-
-#define INSTRUCTIONS "Enter a direction (N, S, E, W, IN, OUT), LOOK to look around, HELP for instructions, or QUIT to quit\nItem commands are TAKE, DROP, EXAMINE followed by the name of the item\nINVENTORY to see what's in your inventory"
+void randomSearch(int gs);					//random search function
+void gridSearch(int gs, int posChecked);	//sequential search function
+void printGrid(int gs);						//print out the grid along with the location of the ship
 
 void main() {
-	readData();		//read in the data to the structs
-	puts(INSTRUCTIONS);
-	input(&locations[1], &locations[1]);	//take user input
+	//initialise the coordinates of the ship
+	srand(time(NULL) * (gridSize - 1));
+	shipX = rand() % gridSize;
+	shipY = rand() % gridSize;
+
+	puts("----------Random Search------------\n");
+	randomSearch(gridSize);
+
+	puts("----------Grid Search----------\n");
+	gridSearch(gridSize, 0);
+
+	printGrid(gridSize);
 }
 
-//read in data from the files
-void readData() {
-	fopen_s(&f, "adventure_locations.txt", "r");
-	char line[101];
-	int i = 1;	//start at 1 to match up with the ids
-	if (f != NULL) {
-		while (!feof(f)) {
-			fgets(line, 101, f);
+void randomSearch(int gs) {
+	int x = 0, y = 0, i = 0;
+	srand(time(NULL) * (gs));
 
-			if (isdigit(line[0])) {		//isdigit() check skips the first two header lines
-				location* l = &locations[i];	//pointer to make the scanning simpler
-
-				sscanf_s(line, "%d\t%d\t%d\t%d\t%d\t%d\t%d\t%[^\n]",
-					&l->id, &l->n, &l->s, &l->e, &l->w, &l->in, &l->out, l->description, 101);	//sscanf_s scans the data into the variables
-
-				i++;
-			}
-		}
-		fclose(f);
+	//finds random coordinates until they both match the ship coordinates
+	while (x != shipX || y != shipY) {
+		if (x != shipX)
+			x = rand() % gs;
+		if (y != shipY)
+			y = rand() % gs;
+		i++;
 	}
 
-	fopen_s(&f, "adventure_objects.txt", "r");
-	i = -2;		//-2 so the first two lines can be skipped
-	if (f != NULL) {
-		while (!feof(f)) {
-			fgets(line, 101, f);
-			if (i >= 0) {	
-				item* item = &items[i];
-				sscanf_s(line, "%[^\t]\t%d\t%[^\n]", item->name, 15, &item->loc, item->des, 50);
-				numItems++;
-			}
-			i++;
-		}
-		fclose(f);
-	}
+	printf("Ship found after %d locations!\n", i);
+	printf("The ship coordinates were (%d, %d)\n", x, y);
 }
 
-//take user input
-void input(location* currL, location* oldL) {
-	if (currL != NULL && currL != &locations[0]) {	//check if the location is valid
-		
-		char inp[50];	//input variable
+void gridSearch(int gs, int posChecked) {
+	int x = posChecked % gs;		//gets the x coordinate between 0 and gridSize - 1
+	int y = (posChecked - x) / gs;	//gets the y coordinate.In an nxn grid, the y will be the number of positions checked, minus the x coordinate (excess) divided by the grid size
 
-		printf("\n%s\n", currL->description);	//print the description of the current location
-		inv(currL, 0, NULL);	//check for and print any items in the area
-
-		scanf_s("%s", inp, 50);
-		lower(inp);
-
-		//check what command was entered
-		switch (parseCom(inp)) {
-		case N:
-			input(&locations[currL->n], currL);		//go north
-			break;
-		case S:
-			input(&locations[currL->s], currL);		//go south
-			break;
-		case E:
-			input(&locations[currL->e], currL);		//go east
-			break;
-		case W:
-			input(&locations[currL->w], currL);		//go west
-			break;
-		case IN:
-			input(&locations[currL->in], currL);	//go in
-			break;
-		case OUT:
-			input(&locations[currL->out], currL);	//go out
-			break;
-		case LOOK:
-			input(currL, oldL);		//look around - reprint location
-			break;
-		case QUIT:
-			puts("Quitting...");	//quit the program
-			break;
-		case HELP:
-			puts(INSTRUCTIONS);		//help - print out the instructions
-			input(currL, oldL);
-			break;
-		case INVENTORY:
-			inv(currL, 4, NULL);	//display the inventory
-			input(currL, oldL);
-			break;
-		case TAKE:
-			inv(currL, 1, itemName);	//take an item
-			input(currL, oldL);
-			break;
-		case DROP:
-			inv(currL, 2, itemName);	//drop an item
-			input(currL, oldL);
-			break;
-		case EXAMINE:
-			inv(currL, 3, itemName);	//examine an item
-			input(currL, oldL);
-			break;
-		default:
-			puts("Invalid Command");
-			input(oldL, oldL);	//run function again with the last location that worked
-			break;
-		}
+	//exit condition
+	if (x == shipX && y == shipY) {
+		printf("Ship found after %d locations!\n", posChecked);
+		printf("The Ships coordinates were (%d, %d)\n\n", x, y);
 	}
 	else {
-		puts("Can't go that way...");
-		input(oldL, oldL);	//run function again with the last location that worked
+		gridSearch(gs, posChecked + 1);	//recursion
 	}
 }
 
-//parse the command into an int
-command parseCom(char input[]) {
-	char curr[15] = "";		//name variable
-
-	scanf_s("%[^\n]s", curr, 15);	//get the remaining part of the entered directive
-	
-	if (strlen(curr) > 1) {		//if it is a relevent entry
-		for (int i = 0; i < strlen(curr); i++)	//this is to remove the extra space at the start of the text
-			curr[i] = curr[i + 1];		//swap the letters to one before
-		curr[strlen(curr)] = '\0';	//replace the end with a \0
-		strcpy_s(itemName, 15, curr);	//set the item name as the name of the item they are trying to interact with
-	}
-	
-	for (int i = 0; i < 13; i++) {
-		if (!strcmp(input, commands[i])) {
-			return (command)i;
-		}
-	}
-	return ERROR;
-}
-
-//inventory function - Modes 0 through 4 perform different functions on items
-void inv(location* l, int mode, char name[]) {
-	for (int i = 0; i < numItems; i++) {
-		item* p = &items[i];	//item pointer
-
-		//carry out the relevent operations based on the mode it was called in
-		switch (mode){
-		case 0:			//Mode 0 - Used to print out descriptions of items if they are in the vicinity
-			if(p->loc == l->id)
-				printf("There is a %s here\n", p->name);
-			break;
-		case 1:			//Mode 1 - Used to take items
-			if (!strcmp(name, p->name) && p->loc == l->id) {
-				p->loc = 0;		//position 0 in the locations used to represent the inventory
-				printf("Took %s\n", p->name);
+void printGrid(int gs) {
+	for (int i = gs; i > 0; i--) {
+		for (int j = gs; j > 0; j--) {
+			//checks if the currently printing position matches the coordinates of the ship
+			if ((i - 1) == shipY && (gs - j) == shipX) {
+				printf("|");
+				printf("\033[0;31m");	//sets text colour to red
+				printf("*");
+				printf("\033[0m");		//sets text colour to default
 			}
-			break;
-		case 2:			//Mode 2 - Used to drop items
-			if (!strcmp(name, p->name) && p->loc == 0) {
-				p->loc = l->id;
-				printf("Dropped %s\n", p->name);
-			}
-			break;
-		case 3:			//Mode 3 - Used to examine an item
-			if (!strcmp(name, p->name) && (p->loc == l->id || p->loc == 0))
-				printf("%s\n", p->des);
-			break;
-		case 4:			//Mode 4 - Used to print items in the inventory
-			if(p->loc == 0)
-				printf("%s\n", p->name);
-			break;
-		default:
-			puts("INVALID MODE");
-			break;
+			else
+				printf("|_");
 		}
-	}
-}
-
-//changes the input to lowercase
-void lower(char arr[]) {
-	for (int i = 0; i < strlen(arr); i++) {
-		arr[i] = tolower(arr[i]);
+		printf("|\n");	//adds the missing line at the end of each row
 	}
 }
